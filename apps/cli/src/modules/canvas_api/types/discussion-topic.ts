b7bb7c0b228c5c22e50d7ce9b4f4443cc19e6700
Wesley Jochman <wjochman@gmail.com>
2023-10-02 09:00:16 -0500

    Finish discussion topic type

--- apps/cli/src/modules/canvas_api/types/discussion-topic.ts
+++ apps/cli/src/modules/canvas_api/types/discussion-topic.ts
@@ -1,89 +1,91 @@
+import { File } from "./file";
+
 export type DiscussionTopic = {
   // The ID of this topic.
-  id: 1;
+  id: number;
   // The topic title.
-  title: "Topic 1";
+  title: string;
   // The HTML content of the message body.
-  message: "<p>content here</p>";
+  message: string;
   // The URL to the discussion topic in canvas.
-  html_url: "https://<canvas>/courses/1/discussion_topics/2";
+  html_url: string;
   // The datetime the topic was posted. If it is null it hasn't been posted yet.
   // (see delayed_post_at)
-  posted_at: "2037-07-21T13:29:31Z";
+  posted_at: Date | null;
   // The datetime for when the last reply was in the topic.
-  last_reply_at: "2037-07-28T19:38:31Z";
+  last_reply_at: Date;
   // If true then a user may not respond to other replies until that user has made
   // an initial reply. Defaults to false.
-  require_initial_post: false;
+  require_initial_post: boolean;
   // Whether or not posts in this topic are visible to the user.
-  user_can_see_posts: true;
+  user_can_see_posts: boolean;
   // The count of entries in the topic.
-  discussion_subentry_count: 0;
+  discussion_subentry_count: number;
   // The read_state of the topic for the current user, 'read' or 'unread'.
-  read_state: "read";
+  read_state: "read" | "unread";
   // The count of unread entries of this topic for the current user.
-  unread_count: 0;
+  unread_count: number;
   // Whether or not the current user is subscribed to this topic.
-  subscribed: true;
+  subscribed: boolean;
   // (Optional) Why the user cannot subscribe to this topic. Only one reason will
   // be returned even if multiple apply. Can be one of: 'initial_post_required':
   // The user must post a reply first; 'not_in_group_set': The user is not in the
   // group set for this graded group discussion; 'not_in_group': The user is not
   // in this topic's group; 'topic_is_announcement': This topic is an announcement
-  subscription_hold: "not_in_group_set";
+  subscription_hold?:
+    | "not_in_group_set"
+    | "not_in_group"
+    | "topic_is_announcement"
+    | "initial_post_required";
   // The unique identifier of the assignment if the topic is for grading,
   // otherwise null.
-  assignment_id: null;
+  assignment_id: number | null;
   // The datetime to publish the topic (if not right away).
-  delayed_post_at: null;
+  delayed_post_at: Date;
   // Whether this discussion topic is published (true) or draft state (false)
-  published: true;
+  published: boolean;
   // The datetime to lock the topic (if ever).
-  lock_at: null;
+  lock_at: Date;
   // Whether or not the discussion is 'closed for comments'.
-  locked: false;
+  locked: boolean;
   // Whether or not the discussion has been 'pinned' by an instructor
-  pinned: false;
+  pinned: boolean;
   // Whether or not this is locked for the user.
-  locked_for_user: true;
+  locked_for_user: boolean;
   // (Optional) Information for the user about the lock. Present when
   // locked_for_user is true.
-  lock_info: null;
+  lock_info?: boolean;
   // (Optional) An explanation of why this is locked for the user. Present when
   // locked_for_user is true.
-  lock_explanation: "This discussion is locked until September 1 at 12:00am";
+  lock_explanation?: string;
   // The username of the topic creator.
-  user_name: "User Name";
+  user_name: string;
   // DEPRECATED An array of topic_ids for the group discussions the user is a part
   // of.
-  topic_children: [5, 7, 10];
+  topic_children: number[];
   // An array of group discussions the user is a part of. Fields include: id,
   // group_id
-  group_topic_children: [
-    { id: 5; group_id: 1 },
-    { id: 7; group_id: 5 },
-    { id: 10; group_id: 4 }
-  ];
+  group_topic_children: { id: number; group_id: number }[];
   // If the topic is for grading and a group assignment this will point to the
   // original topic in the course.
-  root_topic_id: null;
+  root_topic_id: number | null;
   // If the topic is a podcast topic this is the feed url for the current user.
-  podcast_url: "/feeds/topics/1/enrollment_1XAcepje4u228rt4mi7Z1oFbRpn3RAkTzuXIGOPe.rss";
+  podcast_url: string;
   // The type of discussion. Values are 'side_comment', for discussions that only
   // allow one level of nested comments, and 'threaded' for fully threaded
   // discussions.
-  discussion_type: "side_comment";
+  discussion_type: "side_comment" | "threaded";
   // The unique identifier of the group category if the topic is a group
   // discussion, otherwise null.
-  group_category_id: null;
+  group_category_id: number | null;
   // Array of file attachments.
-  attachments: null;
+  attachments: File[];
   // The current user's permissions on this topic.
-  permissions: { attach: true };
+  permissions: Record<string, boolean>;
   // Whether or not users can rate entries in this topic.
-  allow_rating: true;
+  allow_rating: boolean;
   // Whether or not grade permissions are required to rate entries.
-  only_graders_can_rate: true;
+  only_graders_can_rate: boolean;
   // Whether or not entries should be sorted by rating.
-  sort_by_rating: true;
+  sort_by_rating: boolean;
 };
