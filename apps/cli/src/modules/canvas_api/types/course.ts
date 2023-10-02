1923b2abf4df76c0b8de148a45eba9c68f6fe2f4
Wesley Jochman <wjochman@gmail.com>
2023-10-02 08:39:27 -0500

    Finish assignment type

---
+++ apps/cli/src/modules/canvas_api/types/course.ts
@@ -0,0 +1,44 @@
+export type Course = {
+  id: number;
+  name: string;
+  account_id: number;
+  uuid: string;
+  start_at?: string;
+  grading_standard_id?: null;
+  is_public?: null;
+  created_at: string;
+  course_code: string;
+  default_view: string;
+  root_account_id: number;
+  enrollment_term_id: number;
+  license?: null;
+  grade_passback_setting?: null;
+  end_at?: string;
+  public_syllabus: boolean;
+  public_syllabus_to_auth: boolean;
+  storage_quota_mb: number;
+  is_public_to_auth_users: boolean;
+  homeroom_course: boolean;
+  course_color?: null;
+  friendly_name?: null;
+  apply_assignment_group_weights: boolean;
+  calendar: { ics: string };
+  time_zone: string;
+  blueprint: boolean;
+  sis_course_id?: null;
+  sis_import_id?: null;
+  integration_id?: null;
+  enrollments?: Enrollment[] | null;
+  hide_final_grades: boolean;
+  workflow_state: string;
+  restrict_enrollments_to_course_dates: boolean;
+};
+
+export type Enrollment = {
+  type: string;
+  role: string;
+  role_id: number;
+  user_id: number;
+  enrollment_state: string;
+  limit_privileges_to_course_section: boolean;
+};
