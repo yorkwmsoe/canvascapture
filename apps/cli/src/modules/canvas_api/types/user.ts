320275989a8555219d5d67248a9eee80e4c95714
Wesley Jochman <wjochman@gmail.com>
2023-10-02 10:04:58 -0500

    Finish user type

--- apps/cli/src/modules/canvas_api/types/user.ts
+++ apps/cli/src/modules/canvas_api/types/user.ts
@@ -1,52 +1,54 @@
+import { Enrollment } from "./enrollment";
+
 export type User = {
   // The ID of the user.
-  id: 2;
+  id: number;
   // The name of the user.
-  name: "Sheldon Cooper";
+  name: string;
   // The name of the user that is should be used for sorting groups of users, such
   // as in the gradebook.
-  sortable_name: "Cooper, Sheldon";
+  sortable_name: string;
   // The last name of the user.
-  last_name: "Cooper";
+  last_name: string;
   // The first name of the user.
-  first_name: "Sheldon";
+  first_name: string;
   // A short name the user has selected, for use in conversations or other less
   // formal places through the site.
-  short_name: "Shelly";
+  short_name: string;
   // The SIS ID associated with the user.  This field is only included if the user
   // came from a SIS import and has permissions to view SIS information.
-  sis_user_id: "SHEL93921";
+  sis_user_id?: string;
   // The id of the SIS import.  This field is only included if the user came from
   // a SIS import and has permissions to manage SIS information.
-  sis_import_id: 18;
+  sis_import_id?: number;
   // The integration_id associated with the user.  This field is only included if
   // the user came from a SIS import and has permissions to view SIS information.
-  integration_id: "ABC59802";
+  integration_id?: string;
   // The unique login id for the user.  This is what the user uses to log in to
   // Canvas.
-  login_id: "sheldon@caltech.example.com";
+  login_id: string;
   // If avatars are enabled, this field will be included and contain a url to
   // retrieve the user's avatar.
-  avatar_url: "https://en.gravatar.com/avatar/d8cb8c8cd40ddf0cd05241443a591868?s=80&r=g";
+  avatar_url: string;
   // Optional: If avatars are enabled and caller is admin, this field can be
   // requested and will contain the current state of the user's avatar.
-  avatar_state: "approved";
+  avatar_state?: string;
   // Optional: This field can be requested with certain API calls, and will return
   // a list of the users active enrollments. See the List enrollments API for more
   // details about the format of these records.
-  enrollments: null;
+  enrollments?: Enrollment[];
   // Optional: This field can be requested with certain API calls, and will return
   // the users primary email address.
-  email: "sheldon@caltech.example.com";
+  email?: string;
   // Optional: This field can be requested with certain API calls, and will return
   // the users locale in RFC 5646 format.
-  locale: "tlh";
+  locale?: string;
   // Optional: This field is only returned in certain API calls, and will return a
   // timestamp representing the last time the user logged in to canvas.
-  last_login: "2012-05-30T17:45:25Z";
+  last_login?: Date;
   // Optional: This field is only returned in certain API calls, and will return
   // the IANA time zone name of the user's preferred timezone.
-  time_zone: "America/Denver";
+  time_zone?: string;
   // Optional: The user's bio.
-  bio: "I like the Muppets.";
+  bio?: string;
 };
