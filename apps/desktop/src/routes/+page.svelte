d13c3656c33be6764f777093ff7d56de6f6777ce
Wesley Jochman <wjochman@gmail.com>
2023-11-06 11:46:33 -0600

    Configure release

--- apps/desktop/src/routes/+page.svelte
+++ apps/desktop/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    window.location.replace('/setup');
+</script>
+
+<div>Home Page</div>
