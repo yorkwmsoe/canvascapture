392373540428976c1faa53790c494b7bc468a7a6
Wesley Jochman <wjochman.msoe@gmail.com>
2023-11-07 16:41:38 -0600

    Update coverage

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		reporters: ['junit'],
+		outputFile: {
+			junit: 'junit.xml'
+		}
 	}
 });
