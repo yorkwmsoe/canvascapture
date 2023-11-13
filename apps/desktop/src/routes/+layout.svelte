2db665c58f7d7384260ef26f5bf3bf6abc5f4d05
mcdonaldp <mcdonaldp@msoe.edu>
2023-11-13 09:35:10 -0600

    added some ui components

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
 	import '../app.postcss';
 </script>
+<style>
+	h5 {
+		display: inline;
+		margin-right: 20px; /* Adjust the margin as needed */
+	}
+	#sdl-name{
+		margin-right: 80%;
+	}
+</style>
 
 <div class="h-full">
+	<nav>
+		<h5 class="h5" id="sdl-name">Canvas Capture</h5>
+		<!--need to include little icon-->
+		<a href="/settings">
+			<h5 class="h5">Settings</h5>
+		</a>
+	</nav>
 	<slot/>
 </div>
+
--- apps/desktop/src/routes/settings/+page.svelte
+++ apps/desktop/src/routes/settings/+page.svelte
@@ -4,13 +4,13 @@
 <style>
 
 </style>
-
-<div class="set-up-items">
-    <h1 class="h1">Welcome to Canvas Capture</h1>
+<!--NEED TO REMOVE LAYOUT IF POSSIBLE-->
+<div class="settings-items">
+    <h1 class="h1">Settings</h1>
     <input class="input" title="Domain" type="text" placeholder="https://msoe.instructure.com/" />
     <h6>Enter your Canvas Domain</h6>
     <input class="input" title="Access Key" type="text" placeholder="Access Key" />
     <h6>Enter your Canvas Access Key</h6>
-    <button type="button" class="btn variant-filled">Generate</button>
+    <button type="button" class="btn variant-filled">Save</button>
 
 </div>
