ec9b2221426d0ebb8b36fde6482f66e46d68e0a3
sb-msoe <brands@msoe.edu>
2023-11-27 08:16:56 -0600

    Finish home page

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -1,20 +1,25 @@
 <script>
 	import '../app.postcss';
-	import { AppBar } from '@skeletonlabs/skeleton';
 	import "@fortawesome/fontawesome-free/css/all.css"; 
+
+	import { AppBar, Toast } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
 </script>
 
 <style>
 </style>
 
 <div class="h-full">
+	<Toast />
 	<AppBar>
 		<svelte:fragment slot="lead">
 			<a href="/home">
 				<i class="fa-solid fa-home text-2xl" />
 			</a>
 		</svelte:fragment>
-			<span class="bg-gradient-to-br from-red-500 to-black bg-clip-text text-transparent">
+			<span class="gradient-heading">
 				Canvas Capture
 			</span>
 		<svelte:fragment slot="trail">
--- apps/desktop/src/routes/home/+page.svelte
+++ apps/desktop/src/routes/home/+page.svelte
@@ -1,15 +1,22 @@
 <script>
-
+    import { version } from '$app/environment';
 </script>
 <style>
 
 </style>
 
-<div class="set-up-items">
-    <label class="label">
-        <span>Input</span>
-        <input class="input" type="text" placeholder="" />
-    </label>
-    <a class="block card card-hover p-4" href="/elements/cards">Hover Me!</a>
+<div class="h-full grid place-items-center">
+    <form method="GET" class="flex gap-4 flex-col" action="/select">
+        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+            <input type="text" name="generationName" placeholder="Generation Name" required/>
+            <button class="variant-filled-secondary">
+                <i class="fa-solid fa-plus mr-2" />
+                Generate
+            </button>
+        </div>
+    </form>
+</div>
 
+<div class="absolute bottom-0 left-2 h-6 w-16">
+    {version}
 </div>
