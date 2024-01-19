804418b66dbcc46356b1e9a954ce2a0354d0e7fa
Wesley Jochman <jochmanw@msoe.edu>
2024-01-19 11:07:12 -0600

    Create build icon

--- apps/desktop/src/main/index.ts
+++ apps/desktop/src/main/index.ts
@@ -10,7 +10,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
