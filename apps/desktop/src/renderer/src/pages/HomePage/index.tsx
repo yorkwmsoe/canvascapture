10fcf27ed9b6c14873e05c017a911a6d6292e616
sb-msoe <brands@msoe.edu>
2024-02-01 23:10:56 -0600

    Revert home page link addition

--- apps/desktop/src/renderer/src/pages/HomePage/index.tsx
+++ apps/desktop/src/renderer/src/pages/HomePage/index.tsx
@@ -1,10 +1,7 @@
-import { Link } from '@tanstack/react-router'
-
 export function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link to="/settings"> Blog Post </Link>
     </div>
   )
 }
