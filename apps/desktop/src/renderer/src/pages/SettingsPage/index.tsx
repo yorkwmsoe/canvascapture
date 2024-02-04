3e1770d96b39954306b0850d8c980b587401f79e
sb-msoe <brands@msoe.edu>
2024-02-04 11:33:49 -0600

    Go back to home page on save and add header

--- apps/desktop/src/renderer/src/pages/SettingsPage/index.tsx
+++ apps/desktop/src/renderer/src/pages/SettingsPage/index.tsx
@@ -1,5 +1,6 @@
 import { Config } from '@renderer/lib/config'
 import { useSettingsStore } from '@renderer/stores/settings.store'
+import { useNavigate } from '@tanstack/react-router'
 import { Button, Checkbox, Form, Input } from 'antd'
 
 export function SettingsPage() {
@@ -12,11 +13,13 @@ export function SettingsPage() {
     markdownEditor
   } = useSettingsStore()
   const isSetup = false
+  const navigate = useNavigate({ from: isSetup ? '/startup' : '/settings' })
 
   const onFinish = (values: Config) => {
     setCanvasDomain(values.canvasDomain)
     setCanvasAccessToken(values.canvasAccessToken)
     setMarkdownEditor(values.markdownEditor)
+    navigate({ to: '/' })
   }
 
   return (
@@ -24,11 +27,12 @@ export function SettingsPage() {
       name="basic"
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
+      style={{ maxWidth: '90%' }}
       initialValues={{ canvasDomain, canvasAccessToken, markdownEditor }}
       onFinish={onFinish}
       autoComplete="off"
     >
+      <h1>Settings</h1>
       <Form.Item<Config>
         label="Canvas Domain"
         name="canvasDomain"
