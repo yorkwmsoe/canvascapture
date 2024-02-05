ad2bac9719a58e0f764540b0cfef7d75de00dfde
mbeled <mbeled@msoe.edu>
2024-02-04 18:17:10 -0600

    Pushing latest changes to home page

--- apps/desktop/src/renderer/src/pages/HomePage/index.tsx
+++ apps/desktop/src/renderer/src/pages/HomePage/index.tsx
@@ -1,10 +1,11 @@
-import { Input, Layout, Button, Space, Card } from 'antd'
+import {Input, Layout, Button, Space, Card, Flex, Col, Row} from 'antd'
 import { useState } from 'react'
 import { useNavigate } from '@tanstack/react-router'
 import { useGetFolders } from '@renderer/hooks/useGetFolders'
+import { FileMarkdownOutlined } from '@ant-design/icons'
 
 const { Header, Content, Footer } = Layout
-
+const { Meta } = Card;
 export function HomePage() {
   // TODO: Hook up name to the geration store
   const [generationName, setGenerationName] = useState<string>('')
@@ -39,7 +40,7 @@ export function HomePage() {
 
   return (
     <Layout>
-      <Header style={{ backgroundColor: 'white', textAlign: 'left', paddingLeft: '5px' }}>
+      <Header style={{ backgroundColor: 'white', textAlign: 'left', paddingLeft: 5 }}>
         <text style={{ font: '12' }}>
           <b>Canvas Capture</b>
         </text>
@@ -54,49 +55,60 @@ export function HomePage() {
           verticalAlign: 'middle',
           alignItems: 'center',
           height: 'auto',
-          minHeight: '490px'
+          minHeight: '490px',
+          justifyItems: 'center'
         }}
       >
-        <Space.Compact style={{ bottom: '50%' }}>
-          <Input
-            style={{ width: '400px' }}
-            placeholder={'Generation name'}
-            value={generationName}
-            onChange={(e) => setGenerationName(e.target.value)}
-          />
-          <Button onClick={() => generate()} style={{ color: 'white', background: '#051631' }}>
-            Generate
-          </Button>
-        </Space.Compact>
-        <div
-          style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            background: '#051631',
-            color: 'white',
-            borderRadius: '8px',
-            width: '250px',
-            textAlign: 'center'
-          }}
-          className={'modal'}
-          hidden={true}
-          id={'generationModal'}
-        >
-          <div className={'modal-header'}>
+        <div>
+          <Flex align={'center'} justify={'center'}>
             <Space.Compact>
-              <h3>ENTER GENERATION NAME!</h3>
-              <span className={'close'} onClick={() => exitGenerationNameWarning()}>
+              <Input
+                style={{ width: '350px' }}
+                placeholder={'Generation name'}
+                value={generationName}
+                onChange={(e) => setGenerationName(e.target.value)}
+              />
+              <Button onClick={() => generate()} style={{ color: 'white', background: '#051631' }}>
+                Generate
+              </Button>
+            </Space.Compact>
+            <div
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                background: '#051631',
+                color: 'white',
+                borderRadius: '8px',
+                width: '250px',
+                textAlign: 'center'
+              }}
+              className={'modal'}
+              hidden={true}
+              id={'generationModal'}
+            >
+              <div className={'modal-header'}>
+                <Space.Compact>
+                  <h3>ENTER GENERATION NAME!</h3>
+                  <span className={'close'} onClick={() => exitGenerationNameWarning()}>
                 &times;
               </span>
-            </Space.Compact>
-          </div>
-          <div className={'modal-content'}>Please Enter A Name For The Generation.</div>
+                </Space.Compact>
+              </div>
+              <div className={'modal-content'}>Please Enter A Name For The Generation.</div>
+            </div>
+          </Flex>
         </div>
-        <div id={'previousGenerations'}>
-          {folder?.map((folder) => <FolderCard key={folder} folder={folder}></FolderCard>)}
+        <div>
+          <Flex align={'center'} justify={'center'} style={{paddingTop: 15}}>
+            <div>
+              <Row gutter={8}>
+                {folder?.map((folder) => <FolderCard key={folder} folder={folder}></FolderCard>)}
+              </Row>
+            </div>
+          </Flex>
         </div>
       </Content>
-      <Footer style={{ backgroundColor: 'white', color: 'gray', paddingLeft: '5px' }}>
+      <Footer style={{backgroundColor: 'white', color: 'gray', paddingLeft: 5, textAlign: 'left'}}>
         <text>v0.01</text>
       </Footer>
     </Layout>
@@ -109,8 +121,10 @@ type FolderCardProps = {
 
 function FolderCard({ folder }: FolderCardProps) {
   return (
-    <div key={folder}>
-      <Card title={folder} style={{ width: '300' }}></Card>
-    </div>
+    <Col key={folder} span={8}>
+      <Card hoverable={true} style={{ width: 100 , height: 70}} cover={<FileMarkdownOutlined size={15} />}>
+        <Meta title={folder}/>
+      </Card>
+    </Col>
   )
 }
