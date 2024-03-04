c71edb54d1dec32cd2e22676d67e4862b758b5c6
Wesley Jochman <jochmanw@msoe.edu>
2024-03-04 11:09:54 -0600

    Fix cli import

--- apps/cli/src/modules/markdown/generate.ts
+++ apps/cli/src/modules/markdown/generate.ts
@@ -1,14 +1,8 @@
-import { Assignment } from '@canvas-capture/lib/dist/types/canvas_api/assignment'
-import { Course } from '@canvas-capture/lib/dist/types/canvas_api/course'
-import { Submission } from '@canvas-capture/lib/dist/types/canvas_api/submission'
-import { Quiz } from '@canvas-capture/lib/dist/types/canvas_api/quiz'
 import { getQuiz, getSubmissions, getQuizSubmission } from '@modules/canvas_api/api'
-import { generators } from '@canvas-capture/lib'
 import markdownit from 'markdown-it'
 import _ from 'lodash'
 import { mkdirSync, rmSync, writeFileSync } from 'fs'
-
-const { generateAssignment, generateQuiz } = generators
+import { Assignment, Submission, Quiz, generateQuiz, generateAssignment, Course } from '@canvas-capture/lib'
 
 // https://stackoverflow.com/a/70806192
 export const median = (arr: number[]): number => {
