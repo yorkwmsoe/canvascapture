4f875d1e95c2192e6b0a373753d502c52295bdde
mbeled <mbeled@msoe.edu>
2024-05-01 23:05:22 -0500

    Test Cases

--- apps/cli/src/modules/command/commands/tests/assignments.test.ts
+++ apps/cli/src/modules/command/commands/tests/assignments.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it, vi } from 'vitest'
-import {checkbox, getAssignments, getCourses, getSubmissions} from './mocks'
+import { checkbox, getAssignments, getCourses, getSubmissions } from './mocks'
 import { select_assignments } from '../assignments'
 import { testCourses, testAssignments, testSubmissions } from './data'
 import { state } from '@modules/command/state'
@@ -36,79 +36,78 @@ describe('assignments', () => {
         expect(state.assignments).toStrictEqual([])
     })
 
-  it('should log "No assignments found" if no assignments have submissions', async () => {
-      state.courses = testCourses
-      getAssignments.mockReturnValue(Promise.resolve(testAssignments))
-      // Mock getSubmissions to return unsubmitted submissions for all assignments
-      getSubmissions.mockReturnValue(Promise.resolve(testSubmissions.map((x) => ({ ...x, workflow_state: 'unsubmitted' }))))
-      const consoleSpy = vi.spyOn(console, 'log')
+    it('should log "No assignments found" if no assignments have submissions', async () => {
+        state.courses = testCourses
+        getAssignments.mockReturnValue(Promise.resolve(testAssignments))
+        // Mock getSubmissions to return unsubmitted submissions for all assignments
+        getSubmissions.mockReturnValue(Promise.resolve(testSubmissions.map((x) => ({ ...x, workflow_state: 'unsubmitted' }))))
+        const consoleSpy = vi.spyOn(console, 'log')
 
-      await select_assignments()
+        await select_assignments()
 
-      expect(consoleSpy).toHaveBeenCalledWith('No assignments found')
+        expect(consoleSpy).toHaveBeenCalledWith('No assignments found')
     })
 
-  it('should handle undefined state.courses', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
+    it('should handle undefined state.courses', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
 
-    state.courses = undefined
+        state.courses = undefined
 
-    await select_assignments()
+        await select_assignments()
 
-    expect(consoleSpy).toHaveBeenCalledWith('No courses selected')
-  })
+        expect(consoleSpy).toHaveBeenCalledWith('No courses selected')
+    })
 
-  it('should handle null state.courses', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
+    it('should handle null state.courses', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
 
-    state.courses = null
+        state.courses = null
 
-    await select_assignments()
+        await select_assignments()
 
-    expect(consoleSpy).toHaveBeenCalledWith('No courses selected')
-  })
+        expect(consoleSpy).toHaveBeenCalledWith('No courses selected')
+    })
 
-  it('should handle error in getAssignments call', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
-    state.courses = testCourses
+    it('should handle error in getAssignments call', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
+        state.courses = testCourses
 
-    const error = new Error('Failed to fetch assignments')
-    const getAssignmentsMock = vi.fn(() => Promise.reject(error))
-    vi.spyOn(canvasApi, 'getAssignments').mockImplementation(getAssignmentsMock)
+        const error = new Error('Failed to fetch assignments')
+        const getAssignmentsMock = vi.fn(() => Promise.reject(error))
+        vi.spyOn(canvasApi, 'getAssignments').mockImplementation(getAssignmentsMock)
 
-    await select_assignments()
+        await select_assignments()
 
-    expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch assignments')
-  })
+        expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch assignments')
+    })
 
-  it('should handle error in getSubmissions call', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
-    state.courses = testCourses
+    it('should handle error in getSubmissions call', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
+        state.courses = testCourses
 
-    vi.spyOn(canvasApi, 'getAssignments').mockResolvedValue(testAssignments)
+        vi.spyOn(canvasApi, 'getAssignments').mockResolvedValue(testAssignments)
 
-    const error = new Error('Failed to fetch submissions')
-    const getSubmissionsMock = vi.fn(() => Promise.reject(error))
-    vi.spyOn(canvasApi, 'getSubmissions').mockImplementation(getSubmissionsMock)
+        const error = new Error('Failed to fetch submissions')
+        const getSubmissionsMock = vi.fn(() => Promise.reject(error))
+        vi.spyOn(canvasApi, 'getSubmissions').mockImplementation(getSubmissionsMock)
 
-    await select_assignments()
+        await select_assignments()
 
-    expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch submissions')
-  })
+        expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch submissions')
+    })
 
-  it('should print all assignments with checkbox', async () => {
-    const util = require('util')
+    it('should print all assignments with checkbox', async () => {
+        const util = require('util')
 
-    checkbox.mockReturnValue([testAssignments[0].id])
-    getCourses.mockReturnValue(Promise.resolve(testCourses))
-    getAssignments.mockReturnValue(Promise.resolve(testAssignments))
+        checkbox.mockReturnValue([testAssignments[0].id])
+        getCourses.mockReturnValue(Promise.resolve(testCourses))
+        getAssignments.mockReturnValue(Promise.resolve(testAssignments))
 
-    const stdoutBuffer = execSync('bun run src/cli.ts')
+        const stdoutBuffer = execSync('bun run src/cli.ts')
 
-    const stdoutString = stdoutBuffer.toString();
-    console.log('stdoutString:', stdoutString);
+        const stdoutString = stdoutBuffer.toString()
+        console.log('stdoutString:', stdoutString)
 
-    expect(stdoutString).toContain(`[ ] ${testAssignments[0].name}`)
-  })
-
+        expect(stdoutString).toContain(`[ ] ${testAssignments[0].name}`)
+    })
 })
--- apps/cli/src/modules/command/commands/tests/courses.test.ts
+++ apps/cli/src/modules/command/commands/tests/courses.test.ts
@@ -6,96 +6,87 @@ import { state } from '@modules/command/state'
 const { execSync } = require('child_process')
 
 describe('courses', () => {
-  it('should return a course', async () => {
-    checkbox.mockReturnValue([testCourses[0].id])
-    getCourses.mockReturnValue(Promise.resolve(testCourses))
-    getAssignments.mockReturnValue(Promise.resolve(testAssignments))
+    it('should return a course', async () => {
+        checkbox.mockReturnValue([testCourses[0].id])
+        getCourses.mockReturnValue(Promise.resolve(testCourses))
+        getAssignments.mockReturnValue(Promise.resolve(testAssignments))
 
-    await select_courses()
+        await select_courses()
 
-    expect(state.courses).toMatchObject(testCourses)
-  })
-  it('should filter out courses with no assignments', async () => {
-    checkbox.mockReturnValue([testCourses[0].id])
-    getCourses.mockReturnValue(Promise.resolve(testCourses))
-    getAssignments.mockReturnValue(Promise.resolve([]))
-
-    await select_courses()
-
-    expect(state.courses).toBeUndefined()
-  })
-  it('should log no courses found', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
-    checkbox.mockReturnValue([])
-    getCourses.mockReturnValue(Promise.resolve([]))
-    getAssignments.mockReturnValue(Promise.resolve([]))
-
-    await select_courses()
-
-    expect(consoleSpy).toHaveBeenCalledWith('No courses found')
-  })
-
-  it('should handle an error when getting courses', async () => {
-    const consoleSpy = vi.spyOn(console, 'log')
-    // Mock getCourses to throw an error
-    getCourses.mockReturnValue(Promise.reject(new Error('Failed to fetch courses')))
-
-    await select_courses()
-
-    // Check that the function logs the correct error message
-    expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch courses')
-  })
-
-  it('should select courses even if some have no assignments', async () => {
-    // Mock getCourses to return testCourses
-    getCourses.mockReturnValue(Promise.resolve(testCourses))
-    // Mock getAssignments to return testAssignments for the first course and an empty array for the second course
-    getAssignments.mockImplementation((options) => {
-      if (options.courseId === testCourses[0].id) {
-        return Promise.resolve(testAssignments)
-      } else {
-        return Promise.resolve([])
-      }
+        expect(state.courses).toMatchObject(testCourses)
     })
-    // Mock checkbox to select all courses
-    checkbox.mockReturnValue(testCourses.map(c => c.id))
+    it('should filter out courses with no assignments', async () => {
+        checkbox.mockReturnValue([testCourses[0].id])
+        getCourses.mockReturnValue(Promise.resolve(testCourses))
+        getAssignments.mockReturnValue(Promise.resolve([]))
 
-    await select_courses()
+        await select_courses()
 
-    // Check that all courses are selected
-    expect(state.courses).toStrictEqual(testCourses)
-  })
+        expect(state.courses).toBeUndefined()
+    })
+    it('should log no courses found', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
+        checkbox.mockReturnValue([])
+        getCourses.mockReturnValue(Promise.resolve([]))
+        getAssignments.mockReturnValue(Promise.resolve([]))
 
+        await select_courses()
 
+        expect(consoleSpy).toHaveBeenCalledWith('No courses found')
+    })
 
+    it('should handle an error when getting courses', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
+        // Mock getCourses to throw an error
+        getCourses.mockReturnValue(Promise.reject(new Error('Failed to fetch courses')))
 
+        await select_courses()
 
+        // Check that the function logs the correct error message
+        expect(consoleSpy).toHaveBeenCalledWith('Failed to fetch courses')
+    })
 
+    it('should select courses even if some have no assignments', async () => {
+        // Mock getCourses to return testCourses
+        getCourses.mockReturnValue(Promise.resolve(testCourses))
+        // Mock getAssignments to return testAssignments for the first course and an empty array for the second course
+        getAssignments.mockImplementation((options) => {
+            if (options.courseId === testCourses[0].id) {
+                return Promise.resolve(testAssignments)
+            } else {
+                return Promise.resolve([])
+            }
+        })
+        // Mock checkbox to select all courses
+        checkbox.mockReturnValue(testCourses.map((c) => c.id))
 
-  it('should print all courses with checkbox', async () => {
-    //const consoleSpy = vi.spyOn(console, 'log');
-    const util = require('util');
-    // Assuming checkbox returns an array of selected course IDs
-    checkbox.mockReturnValue([testCourses[0].id]);
+        await select_courses()
 
-    getCourses.mockReturnValue(Promise.resolve(testCourses));
-    getAssignments.mockReturnValue(Promise.resolve(testAssignments));
+        // Check that all courses are selected
+        expect(state.courses).toStrictEqual(testCourses)
+    })
 
-    await select_courses();
-    //console.log('Logged arguments:', util.inspect(consoleSpy.mock.calls, { depth: null }));
-    const stdoutBuffer = execSync('bun run src/cli.ts');
+    it('should print all courses with checkbox', async () => {
+        //const consoleSpy = vi.spyOn(console, 'log');
+        const util = require('util')
+        // Assuming checkbox returns an array of selected course IDs
+        checkbox.mockReturnValue([testCourses[0].id])
 
+        getCourses.mockReturnValue(Promise.resolve(testCourses))
+        getAssignments.mockReturnValue(Promise.resolve(testAssignments))
 
-    // Assert that console.log was called with the expected output
-    //expect(consoleSpy).toBe(`[ ] ${testCourses[0].name}`);
-    const stdoutString = stdoutBuffer.toString();
-    console.log('Captured stdout:', stdoutString);
+        await select_courses()
+        //console.log('Logged arguments:', util.inspect(consoleSpy.mock.calls, { depth: null }));
+        const stdoutBuffer = execSync('bun run src/cli.ts')
 
-    expect(stdoutString).toContain(`[ ] ${testCourses[0].name}`);
+        // Assert that console.log was called with the expected output
+        //expect(consoleSpy).toBe(`[ ] ${testCourses[0].name}`);
+        const stdoutString = stdoutBuffer.toString()
+        console.log('Captured stdout:', stdoutString)
 
+        expect(stdoutString).toContain(`[ ] ${testCourses[0].name}`)
 
-    // Restore the original implementation of console.log
-    //consoleSpy.mockRestore();
-  });
-
+        // Restore the original implementation of console.log
+        //consoleSpy.mockRestore();
+    })
 })
